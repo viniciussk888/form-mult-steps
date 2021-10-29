1679f6683dbe1ca41b1b559744a069c129fa0ff3
vinicius martins <vinicius.cross07@gmail.com>
2021-10-29 10:10:14 -0300

    feat:form1

--- src/pages/formStep1/index.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-import * as C from "./styles";
-
-export const FormStep1: React.FC = () => {
-  return <C.Container>Passo 1</C.Container>;
-};
--- src/router.tsx
+++ src/router.tsx
@@ -1,9 +1,9 @@
 import { BrowserRouter, Route } from "react-router-dom";
 
 //pages
-import { FormStep1 } from "./pages/formStep1";
-import { FormStep2 } from "./pages/formStep2";
-import { FormStep3 } from "./pages/formStep3";
+import { FormStep1 } from "./pages/FormStep1";
+import { FormStep2 } from "./pages/FormStep2";
+import { FormStep3 } from "./pages/FormStep3";
 
 export const Router = () => {
   return (
